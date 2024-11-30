0bd90f0012292c45ab7f5dc296afcc0fa3f0ce1c
Olateru Olu <oluolateru@gmail.com>
2024-11-30 15:16:13 +0100

    Finished

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const graphqlHttp = require('express-graphql').graphqlHTTP;
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -55,6 +56,21 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: 'File stored.', filePath: req.file.path });
+});
+
 app.use(
   '/graphql',
   graphqlHttp({
@@ -89,3 +105,5 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
+
+
